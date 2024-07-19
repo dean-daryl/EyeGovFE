a3c7c314e681e3762b568a00e3ae0c624c066846
dean-daryl <deandaryl7@gmail.com>
2024-07-19 20:08:04 +0200

    fix deployment

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+
+const App = React.lazy(() => import('./App'));
+
+const container = document.getElementById('root')!;
+
+ReactDOMClient.createRoot(container).render(<App />);
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,11 @@
-import {useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Home from '../pages/Home';
 
- export default function Navigation() {
-   let element = useRoutes([
-     { path: '/', element: <Home /> },
+export default function Navigation() {
+  let element = useRoutes([
+    { path: '/', element: <Home /> },
     //  { path: '/about', element: <About /> },
-   ]);
+  ]);
 
-   return element;
- }
+  return element;
+}
