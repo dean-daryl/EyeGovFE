3d0da996496f0740f281ecb8c4384336fe72b964
dean-daryl <deandaryl7@gmail.com>
2024-07-28 00:34:34 +0200

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import * as ReactDOMClient from 'react-dom/client';
 import '../src/styles/index.css';
 import '../node_modules/froala-editor/css/froala_style.min.css';
 import '../node_modules/froala-editor/css/froala_editor.pkgd.min.css';
+import '../node_modules/froala-editor/js/plugins.pkgd.min.js';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
